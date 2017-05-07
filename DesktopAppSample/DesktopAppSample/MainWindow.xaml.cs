ebdec1073a9e5143a656b67927acc6118ea711c1
Davide Zordan <davide.zordan@microsoft.com>
2017-05-07 19:18:30 +0100

    Adding Simple WPF demo

---
+++ DesktopAppSample/DesktopAppSample/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using MahApps.Metro.Controls;
+
+namespace DesktopAppSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var helpers = new DesktopBridge.Helpers();
+            if (helpers.IsRunningAsUwp())
+            {
+                PackageNameText.Text = Windows.ApplicationModel.Package.Current.DisplayName;
+            } else
+            {
+                PackageNameText.Text = "Not running as UWP package";
+            }
+        }
+    }
+}
